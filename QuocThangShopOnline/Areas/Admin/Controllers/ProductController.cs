11669ee588685cca279e0164a1e4f4761684f2c8
TECHVN\nhanddt <nhanddt@mti-tech.vn>
2022-04-08 10:00:37 +0700

    commit

--- QuocThangShopOnline/Areas/Admin/Controllers/ProductController.cs
+++ QuocThangShopOnline/Areas/Admin/Controllers/ProductController.cs
@@ -33,6 +33,7 @@ namespace QuocThangShopOnline.Areas.Admin.Controllers
         [HttpPost]
         public  IActionResult Create(ProductViewModel model)
         {
+
             if (!ModelState.IsValid)
             {
                 return BadRequest();
